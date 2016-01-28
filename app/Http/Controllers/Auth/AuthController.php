d0a32999c52338dd037a8a43ec1905e951053b82
James Fairhurst <info@jamesfairhurst.co.uk>
2016-01-28 13:42:56 +0000

    Formatting

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -111,9 +111,9 @@ class AuthController extends Controller
 
         return User::create([
             'spotify_id' => $spotifyUser['id'],
-            'name' => $spotifyUser['display_name'],
-            'avatar' => ((isset($spotifyUser['images'][0]['url'])) ? $spotifyUser['images'][0]['url'] : ''),
-            'token' => json_encode($spotifyToken),
+            'name'       => $spotifyUser['display_name'],
+            'avatar'     => ((isset($spotifyUser['images'][0]['url'])) ? $spotifyUser['images'][0]['url'] : ''),
+            'token'      => json_encode($spotifyToken),
         ]);
     }
 
