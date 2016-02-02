0c27bb48d1280223e6cc819f505b51a9552151d9
James Fairhurst <info@jamesfairhurst.co.uk>
2016-02-02 19:53:49 +0000

    Bind Spotify Provider to the IOC

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -33,16 +33,7 @@ class AuthController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
 
-        $this->provider = new \League\OAuth2\Client\Provider\GenericProvider([
-            'clientId'                => env('SPOTIFY_CLIENT_ID'),
-            'clientSecret'            => env('SPOTIFY_SECRET'),
-            'redirectUri'             => env('SPOTIFY_REDIRECT_URI'),
-            'urlAuthorize'            => 'https://accounts.spotify.com/authorize',
-            'urlAccessToken'          => 'https://accounts.spotify.com/api/token',
-            'urlResourceOwnerDetails' => 'https://api.spotify.com/v1/me',
-            'scopes'                  => ['playlist-modify-public', 'user-library-read'],
-            'scopeSeparator'          => ' ',
-        ]);
+        $this->provider = \App::make('League\OAuth2\Client\Provider\GenericProvider');
     }
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use League\OAuth2\Client\Provider\GenericProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(GenericProvider::class, function ($app) {
+            return new GenericProvider([
+                'clientId'                => env('SPOTIFY_CLIENT_ID'),
+                'clientSecret'            => env('SPOTIFY_SECRET'),
+                'redirectUri'             => env('SPOTIFY_REDIRECT_URI'),
+                'urlAuthorize'            => 'https://accounts.spotify.com/authorize',
+                'urlAccessToken'          => 'https://accounts.spotify.com/api/token',
+                'urlResourceOwnerDetails' => 'https://api.spotify.com/v1/me',
+                'scopes'                  => ['playlist-modify-public', 'user-library-read'],
+                'scopeSeparator'          => ' ',
+            ]);
+        });
     }
 }
