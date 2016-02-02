b39e21735b41e190833548e7657b45430c250563
James Fairhurst <info@jamesfairhurst.co.uk>
2016-02-02 19:56:01 +0000

    Update Views

---
+++ resources/views/playlist.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="page-header">
+        <h1>Your Playlist: {{ $playlist->name }}</h1>
+    </div>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Rules &amp; Info</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($playlist->rules as $rule): ?>
+            <tr>
+                <td>{{ $rule->key_nice }} <span class="text-muted">{{ $rule->comparison_operator_nice }}</span> <strong>{{ $rule->value }}</strong></td>
+            </tr>
+            <?php endforeach; ?>
+            <tr>
+                <td>Order by <strong>{{ $playlist->order_nice }}</strong></td>
+            </tr>
+            <tr>
+                <td>Limit <strong>{{ ($playlist->limit) ? $playlist->limit : 'n/a' }}</strong></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Track</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Added At</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($tracks as $track): ?>
+            <tr>
+                <td>{{ str_limit($track->name, 50) }}</td>
+                <td>{{ $track->artist }}</td>
+                <td>{!! $track->album->name . '<br/><small>' . $track->album->released_at->format('m/Y') . '</small>' !!}</td>
+                <td>{{ $track->added_at->format('d-m-Y H:i') }}</td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</div>
+@endsection
--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -19,7 +19,7 @@
                     <label for="playlist-name" class="col-sm-3 control-label">Name</label>
 
                     <div class="col-sm-6">
-                        <input type="text" name="name" id="playlist-name" class="form-control" value="{{ old('name') }}">
+                        <input type="text" name="name" id="playlist-name" class="form-control" value="{{ old('name') }}" placeholder="This is not The Greatest Playlist in the World...">
                     </div>
                 </div>
 
@@ -38,20 +38,20 @@
                     </div>
                     <div class="col-sm-2">
                         <select name="rule[0][comparison_operator]" class="form-control">
-                            <option value="contains">contains</option>
-                            <option value="not_contains">does not contain</option>
-                            <option value="=">is</option>
-                            <option value="!=">is not</option>
-                            <option value="begins_with">begins with</option>
-                            <option value="ends_with">ends with</option>
-                            <option value=">">is greater than</option>
-                            <option value="<">is less than</option>
-                            <option value=">">is after</option>
-                            <option value="<">is before</option>
+                            <option value="contains">contains</option>{{-- artist|album --}}
+                            <option value="not_contains">does not contain</option>{{-- artist|album --}}
+                            <option value="=">is</option>{{-- artist|album|date_added|year --}}
+                            <option value="!=">is not</option>{{-- artist|album|date_added|year --}}
+                            <option value="begins_with">begins with</option>{{-- artist|album --}}
+                            <option value="ends_with">ends with</option>{{-- artist|album --}}
+                            <option value=">">is greater than</option>{{-- year --}}
+                            <option value="<">is less than</option>{{-- year --}}
+                            <option value=">">is after</option>{{-- date_added --}}
+                            <option value="<">is before</option>{{-- date_added --}}
                         </select>
                     </div>
                     <div class="col-sm-3">
-                        <input type="text" name="rule[0][value]" class="form-control">
+                        <input type="text" name="rule[0][value]" class="form-control" placeholder="Tenacious D">
                     </div>
                     <div class="col-sm-1">
                         <button type="button" class="btn btn-default btn-success" aria-label="Left Align">
@@ -62,6 +62,45 @@
                         </button>
                     </div>
                 </div>
+
+                <div class="form-group">
+                    <label for="playlist-limit" class="col-sm-3 control-label">Limit?</label>
+
+                    <div class="col-sm-6">
+                        <input type="text" name="limit" id="playlist-limit" class="form-control" value="{{ old('limit') }}" placeholder="Leave blank for no limit">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="playlist-order" class="col-sm-3 control-label">Order</label>
+
+                    <div class="col-sm-6">
+                        {{-- <input type="text" name="order" id="playlist-order" class="form-control" value="{{ old('order') }}"> --}}
+                        <select name="order" id="playlist-order" class="form-control">
+                            <option value="added_desc">Date Added Desc</option>
+                            <option value="added_asc">Date Added Asc</option>
+                            <option value="album">Album</option>
+                            <option value="artist">Artist</option>
+                            <option value="name">Name</option>
+                            <option value="year_desc">Year Desc</option>
+                            <option value="year_asc">Year Asc</option>
+                            <option value="random">Random</option>
+                            {{-- <option value="popularity_desc">Popularity Desc</option> --}}
+                            {{-- <option value="popularity_asc">Popularity Asc</option> --}}
+                            {{-- <option value="played_desc">Played Desc</option> --}}
+                            {{-- <option value="played_asc">Played Asc</option> --}}
+                            {{-- <option value="genre">Genre</option> --}}
+                        </select>
+                    </div>
+                </div>
+
+                {{-- <div class="form-group">
+                    <label for="playlist-live" class="col-sm-3 control-label">Live updating?</label>
+
+                    <div class="col-sm-6">
+                        <input type="checkbox" name="live" id="playlist-live">
+                    </div>
+                </div> --}}
 
                 <div class="form-group">
                     <div class="col-sm-offset-3 col-sm-6">
@@ -89,7 +128,13 @@
                     <td>{{ str_limit($playlist->name, 50) }}</td>
                     <td>{{ $playlist->created_at->format('d-m-Y H:i') }}</td>
                     <td>
-                        <form action="{{ url('playlist/' . $playlist->id) }}" method="POST">
+                        <a href="{{ url('/playlist', [$playlist->id]) }}" class="btn btn-default">View</a>
+                        <form action="{{ url('playlist/' . $playlist->id) }}" method="POST" class="visible-xs-inline visible-sm-inline visible-md-inline visible-lg-inline">
+                            {!! csrf_field() !!}
+
+                            <button class="btn btn-default">Push</button>
+                        </form>
+                        <form action="{{ url('playlist/' . $playlist->id) }}" method="POST" class="visible-xs-inline visible-sm-inline visible-md-inline visible-lg-inline">
                             {!! csrf_field() !!}
                             {!! method_field('DELETE') !!}
 
