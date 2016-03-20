aed66b037ecd20b9a6a5db6102a41a5feda894ea
James Fairhurst <info@jamesfairhurst.co.uk>
2016-03-20 20:26:11 +0000

    Cleanup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,10 +72,7 @@
                 @endif
 
                 <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::guest())
-                        {{-- <li><a href="{{ url('/auth/spotify') }}">Login</a></li> --}}
-                        {{-- <li><a href="{{ url('/register') }}">Register</a></li> --}}
-                    @else
+                    @if (!Auth::guest())
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
