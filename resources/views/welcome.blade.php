38008851c6420463dcc801b0822220a3210bfdbe
James Fairhurst <info@jamesfairhurst.co.uk>
2016-03-31 10:46:11 +0100

    Updated Welcome page
    - Added Screenshots

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,13 +14,29 @@
 <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-4 text-center">
           <h2>Features</h2>
           <p>Create Smart Playlists with Track/Album rules and push them to Spotify from your list of saved Tracks.</p>
-          {{-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> --}}
+          <div class="row">
+            <div class="col-xs-6 col-md-4">
+              <a href="{{ asset('img/screenshot-01.png') }}" class="thumbnail">
+                <img src="{{ asset('img/screenshot-01-thumbnail.png') }}" class="img-responsive img-rounded" alt="">
+              </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+              <a href="{{ asset('img/screenshot-02.png') }}" class="thumbnail">
+                <img src="{{ asset('img/screenshot-02-thumbnail.png') }}" class="img-responsive img-rounded" alt="">
+              </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+              <a href="{{ asset('img/screenshot-03.png') }}" class="thumbnail">
+                <img src="{{ asset('img/screenshot-03-thumbnail.png') }}" class="img-responsive img-rounded" alt="">
+              </a>
+            </div>
+          </div>
         </div>
         <div class="col-md-4">
-          <h2>Todo</h2>
+          <h2 class=" text-center">Todo</h2>
           <ul>
             <li>Live updating in the background so your Playlists are kept upto date</li>
             <li>Additional Playlist Rules</li>
@@ -28,13 +44,11 @@
             <li>Display live Tracks when creating a new Playlist</li>
             <li>Use Tracks from all your Playlists not just your saved Tracks</li>
           </ul>
-          {{-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> --}}
-       </div>
-        <div class="col-md-4">
+        </div>
+        <div class="col-md-4 text-center">
           <h2>About</h2>
           <p>Coming from iTunes and being able to easily create Smart Playlists I became frustrated with not being able to create them in Spotify so I made this.</p>
           <p>All feedback and suggestions welcome just <a href="mailto:support@smartplaylistsforspotify.co.uk">email me</a>.</p>
-          {{-- <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p> --}}
         </div>
       </div>
 </div>
