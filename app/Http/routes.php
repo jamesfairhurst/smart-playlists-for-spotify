69564a427d3cf75a65ab077e25a3fff1a4181045
James Fairhurst <info@jamesfairhurst.co.uk>
2016-04-08 20:44:49 +0100

    Cleanup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,23 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
+use Carbon\Carbon;
 use App\Track;
 use App\Artist;
 use App\Album;
 use App\Playlist;
-use Illuminate\Http\Request;
-use Carbon\Carbon;
-
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 
 /*
 |--------------------------------------------------------------------------
