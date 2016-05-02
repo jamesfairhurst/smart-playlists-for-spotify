86efe6090cefb997033a28627e7bf1d62d536c31
James Fairhurst <info@jamesfairhurst.co.uk>
2016-05-02 11:40:15 +0100

    Added GA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <script src="https://js.pusher.com/3.0/pusher.min.js"></script>
     <script>
     (function () {
-        var pusher = new Pusher('{{ env("PUSHER_KEY") }}', {
+        var pusher = new Pusher('{{ env('PUSHER_KEY') }}', {
             cluster: 'eu',
             encrypted: true
         });
@@ -104,5 +104,17 @@
     <script src="{{ asset('js/bootstrap-notify.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    @if (env('APP_ENV') != 'local')
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-1927080-9', 'auto');
+    ga('send', 'pageview');
+    </script>
+    @endif
 </body>
 </html>
