9c54f6a72d896eebde9ef296a83f3d2c34e108d9
James Fairhurst <info@jamesfairhurst.co.uk>
2016-05-10 15:22:35 +0100

    Fix: Typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,7 @@
 
     <footer class="footer">
         <div class="container">
-            <p class="text-muted text-center">Made with <a href="https://laravel.com" target="_blank">Laravel</a> on <a href="https://forge.laravel.com" target="_blank">Forge</a> by <a href="http://www.jamesfairhurst.co.uk" target="_blank">James Fairhurst</a> fueled by <a href="https://open.spotify.com/user/james_f" target="_blank"><i class="fa fa-music"></i></a> <a href="https://pactcoffee.com/?voucher=JAMES-XIKO8G" target="_blank"><i class="fa fa-coffee"></i></a> <a href="http://www.bakingbad.co.uk/2013/07/my-favourite-carrot-cake/" target="_blank"><i class="fa fa-birthday-cake"></i></a> <a href="http://www.leffe.com/en/beers/leffe-blond" target="_blank"><i class="fa fa-beer"></i></a></p>
+            <p class="text-muted text-center">Made with <a href="https://laravel.com" target="_blank">Laravel</a> on <a href="https://forge.laravel.com" target="_blank">Forge</a> by <a href="http://www.jamesfairhurst.co.uk" target="_blank">James Fairhurst</a> fuelled by <a href="https://open.spotify.com/user/james_f" target="_blank"><i class="fa fa-music"></i></a> <a href="https://pactcoffee.com/?voucher=JAMES-XIKO8G" target="_blank"><i class="fa fa-coffee"></i></a> <a href="http://www.bakingbad.co.uk/2013/07/my-favourite-carrot-cake/" target="_blank"><i class="fa fa-birthday-cake"></i></a> <a href="http://www.leffe.com/en/beers/leffe-blond" target="_blank"><i class="fa fa-beer"></i></a></p>
         </div>
     </footer>
 
